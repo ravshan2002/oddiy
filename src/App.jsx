a75e44ae45d9b67579cb029d639b3a006533364d
ravshan2002 <xojamqulovravshan731@gmail.com>
2024-04-27 13:28:15 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1 className='text-center text-rose-600 text-3xl font-bold mt-36'>Tailwindcssdagi birinchi projectim</h1>
+    </div>
+  )
+}
+
+export default App

939e20035d3ae643d0d7b88f0f21b26074155a2c
ravshan2002 <xojamqulovravshan731@gmail.com>
2024-04-27 13:33:53 +0500

    change new text

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div>
       <h1 className='text-center text-rose-600 text-3xl font-bold mt-36'>Tailwindcssdagi birinchi projectim</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quisquam eum, quo cupiditate eius sequi delectus nam. Ipsam repellat facere, modi debitis, ipsa nam quam, odit accusantium vero distinctio atque obcaecati nihil tempore esse. Error debitis, assumenda recusandae repellat impedit quia laudantium dolores magnam consequatur quae fuga iure ducimus, iste cum officia, sed sunt beatae temporibus omnis vitae earum similique quaerat reiciendis voluptate! Possimus ullam aut ipsam, a aliquam sit rerum earum soluta, praesentium odio vero deserunt fuga vitae, nesciunt exercitationem illum id assumenda ab perspiciatis consectetur corporis quasi aperiam distinctio. Praesentium quo laborum quibusdam tempora fugiat blanditiis atque temporibus.</p>
     </div>
   )
 }
